ae02751194380e404f55d64595b4371d2e364dc8
Bonevy BEBY <bonevybeby@gmail.com>
2024-02-20 17:17:31 +0100

    added complete CRUD

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-//Author: Bonevy BEBY
-//var http=require('http');
-//http module can create a http server that listens to the server port and gives
-//the response back to the client
-/*http.createServer(function(req, res){
-    res.write("welcome to node world!");
-    res.end();
-}).listen(8080);*/
-const express= require('express');
-const app=express();
-/*app.get("/",function (req, resp) {
-    resp.send("Welcome to express js");
-})*/
-//added for api
+const express=require('express');
+const app= express();
+
+
+//app.get('/',(req, res)=>{
+    //res.json({message:"Your request is sent successfull!"});
+  //  res.send("Your request is sent successfull!");
+//})
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use('api/users',require('./routes/api/users'));
-//end added api
-const port=3000;
-app.listen(port,()=>{
-    console.log("Server start on port "+port)
+
+app.listen(3005,()=>{
+    console.log("Server started on port 3005");
 })
-
-
